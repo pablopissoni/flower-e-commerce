af49d317b495541a0a7362ba43fe73ee0682a751
pablopissoni <pablopissoni@gmail.com>
2025-01-16 13:34:27 -0300

    feat: se agrego una vista para Page not found

--- src/components/ui/not-found/PageNotFound.tsx
+++ src/components/ui/not-found/PageNotFound.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="flex flex-col-reverse md:flex-row h-[800px] w-full justify-center items-center align-middle">
+      <div className="text-center px-5 mx-5">
+        <h2 className={`&{titleFont.className} antialiased text-9xl`}>404</h2>
+        <p className="font-semibold text-xl">La página que buscas no existe</p>
+        <Link href="/" className="mt-5 text-xl hover:text-blue-400">
+          Volver a la página principal
+        </Link>
+      </div>
+    </div>
+  );
 };
