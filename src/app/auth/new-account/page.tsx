bf337b50e3085bb68e16653233e0c5d4a3ebe1da
pablopissoni <pablopissoni@gmail.com>
2025-01-21 15:39:20 -0300

    add: page de Login y New-Account maquetacion

--- src/app/auth/new-account/page.tsx
+++ src/app/auth/new-account/page.tsx
@@ -1,9 +1,34 @@
 import { titleFont } from "@/config/fonts";
+import Link from "next/link";
 
-export default function Home() {
+export default function NewAccount() {
   return (
-    <div>
-      <h1 className={titleFont.className}>Flower e-commerce new account</h1>
+    <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
+      <h1 className={`${titleFont.className} text-4xl mb-5 antialiased font-semibold`}>Nueva Cuenta</h1>
+
+      <div className="flex flex-col">
+        <label htmlFor="texto">Nombre Completo</label>
+        <input className="px-5 py-2 border bg-gray-200 rounded mb-5" type="texto" />
+
+        <label htmlFor="email">Correo electrónico</label>
+        <input className="px-5 py-2 border bg-gray-200 rounded mb-5" type="email" />
+
+        <label htmlFor="email">Contraseña</label>
+        <input className="px-5 py-2 border bg-gray-200 rounded mb-5" type="email" />
+
+        <button className="btn-primary">Crear cuenta</button>
+
+        {/* divisor l ine */}
+        <div className="flex items-center my-5">
+          <div className="flex-1 border-t border-gray-500"></div>
+          <div className="px-2 text-gray-800">O</div>
+          <div className="flex-1 border-t border-gray-500"></div>
+        </div>
+
+        <Link href="/auth/login" className="btn-secondary text-center">
+          Ingresar
+        </Link>
+      </div>
     </div>
   );
 }
