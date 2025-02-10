32671475b3bad55d4304d607178b901fe28687d4
pablopissoni <pablopissoni@gmail.com>
2025-02-09 22:05:19 -0300

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,16 @@
-export * from "./ui/top-menu/TopMenu";
-export * from "./ui/not-found/PageNotFound";
-export * from "./ui/title/Title";
+// PRODUCTS
 export * from "./products/product-grid/ProductGrid";
 export * from "./products/product-grid/ProductGridItem";
-export * from "./ui/sidebar/Sidebar";
+// PRODUCT
+export * from "./product/stock-label/StockLabel";
 export * from "./product/size-selector/SizeSelector";
 export * from "./product/quntity-selector/QuantitySelector";
 export * from "./product/slideshow/ProductSlideshow";
 export * from "./product/slideshow/ProductMobileSlideshow";
+// UI
+export * from "./ui/sidebar/Sidebar";
+export * from "./ui/top-menu/TopMenu";
+export * from "./ui/not-found/PageNotFound";
+export * from "./ui/title/Title";
 export * from "./ui/footer/Footer";
 export * from "./ui/pagination/Pagination";
